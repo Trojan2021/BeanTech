90bb19723c11e26c1bff67e5924f2ddf4fa62cd1
Sawyer Ramsey <sawyereramsey@gmail.com>
2023-04-06 11:50:48 -0400

    Finalized Version

--- conditionTest.py
+++ conditionTest.py
@@ -17,22 +17,27 @@ def runSim():
     Room4Temp = int(room4Temp.get())
     DesiredTemp = int(desiredTemp.get())
     OutsideTemp = int(outsideTemp.get())
+    # Exterior windows
     r1w = False
     r2w = False
     r3w = False
     r4w = False
+    # Interior windows connecting rooms
     r12w = False
     r23w = False
     r34w = False
     r41w = False
+    # Interior fans (True is counter-clockwise, False is clockwise)
     r12Fan = False
     r23Fan = False
     r34Fan = False
     r41Fan = False
+    # Room heaters
     r1Heat = False
     r2Heat = False
     r3Heat = False
     r4Heat = False
+    # Room air conditioning
     r1Ac = False
     r2Ac = False
     r3Ac = False
